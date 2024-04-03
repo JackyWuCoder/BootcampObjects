616c267a2c689648be0e302fc461ab1be91a79c6
JackyWuCoder <JackyWuCoder@gmail.com>
2024-04-02 17:47:14 -0700

    "Health" Class Variables
    1) currentHealth, maxHealth, healthRegenRate

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour
+public class Health
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private float currentHealth;
+    private float maxHealth;
+    private float healthRegenRate;
 }
