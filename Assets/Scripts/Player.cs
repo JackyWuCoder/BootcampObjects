bcb4fa478c0d7905dd95f2593717e878de401a17
JackyWuCoder <JackyWuCoder@gmail.com>
2024-04-02 17:44:22 -0700

    Player Methods
    1) Move(), Shoot(), Die()
    2) Removed inheritance from MonoBehaviour (removed Start(), Update() methods)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player
 {
-    // Start is called before the first frame update
-    void Start()
+    private string nickName;
+    private float speed;
+
+    public void Move()
     {
-        
+        Debug.Log("Player is moving");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Shoot()
     {
-        
+        Debug.Log("Player shoots");
+    }
+
+    public void Die()
+    {
+        Debug.Log("Player died");
     }
 }
