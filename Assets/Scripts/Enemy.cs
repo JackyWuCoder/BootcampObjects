a8c8e57649db3efad5318a59a570c1c8f04b708e
JackyWuCoder <JackyWuCoder@gmail.com>
2024-04-09 11:33:31 -0700

    Update Enemy.cs
    Declared methods of Move, Shoot, Attack, Die with parameters for the Enemy Class.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,4 +10,33 @@ public class Enemy
     public Health health = new Health();
     // Why do we not instantiate the weapon here?
     public Weapon weapon;
+
+    private EnemyType enemyType;
+
+    private enum TestEnum
+    { 
+        value1, value2, value3
+    }
+
+    private TestEnum testEnum;
+
+    public void Move(Transform target) 
+    { 
+    
+    }
+
+    public void Shoot(Vector3 direction, float speed)
+    { 
+    
+    }
+
+    public void Attack(float interval)
+    { 
+        
+    }
+
+    public void Die(string message)
+    {
+        Debug.Log($"Enemy died with a message {message}");
+    }
 }
