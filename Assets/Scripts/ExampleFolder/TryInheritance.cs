9c1bc5afb38aed8fee9b973e5c453a35e1277175
JackyWuCoder <JackyWuCoder@gmail.com>
2024-04-10 16:03:50 -0700

    Update TryInheritance.cs

--- Assets/Scripts/ExampleFolder/TryInheritance.cs
+++ Assets/Scripts/ExampleFolder/TryInheritance.cs
@@ -7,7 +7,7 @@ public class TryInheritance : RandomCalculations
     // Start is called before the first frame update
     void Start()
     {
-        
+        CalcRandNum();
     }
 
     // Update is called once per frame
