ef755040d6af679fb636190d4a2856df1d62349d
JackyWuCoder <JackyWuCoder@gmail.com>
2024-04-11 15:15:51 -0700

    Enemy Movements
    1) Enemy rotation and translations.
    2) All enemies have the same speed.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Enemy : PlayableObject
 {
     private string name;
-    private float speed;
+    [SerializeField] protected float speed;
 
     private EnemyType enemyType;
 
@@ -19,6 +19,21 @@ public class Enemy : PlayableObject
     public override void Move(Vector2 direction, Vector2 target)
     {
         throw new System.NotImplementedException();
+    }
+
+    public override void Move(float speed)
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    public override void Move(Vector2 direction)
+    {
+        direction.x -= transform.position.x;
+        direction.y -= transform.position.y;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
     }
 
     public override void Shoot()
--- Assets/Scripts/PlayableObject.cs
+++ Assets/Scripts/PlayableObject.cs
@@ -10,6 +10,18 @@ public abstract class PlayableObject : MonoBehaviour, IDamageable
     public Weapon weapon;
 
     public abstract void Move(Vector2 direction, Vector2 target);
+
+    // Polymorphism/method overloads
+    public virtual void Move(Vector2 direction)
+    { 
+        
+    }
+
+    public virtual void Move(float speed)
+    { 
+    
+    }
+
     public abstract void Shoot();
     public abstract void Attack(float interval);
     public abstract void Die();
