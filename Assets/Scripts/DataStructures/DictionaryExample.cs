d2e2772a3c9158157bbcdaf98b8fce7261c52b14
JackyWuCoder <JackyWuCoder@gmail.com>
2024-04-12 16:29:18 -0700

    Learning Dictionaries

---
+++ Assets/Scripts/DataStructures/DictionaryExample.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DictionaryExample : MonoBehaviour
+{
+    public Dictionary<string, int> dictionary = new Dictionary<string, int>();
+    [SerializeField] private TMP_Text txtTriangle, txtSquare, txtCircle;
+    public string checkForKey = "Rectangle";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dictionary.Add("Triangles", 0);
+        dictionary.Add("Squares", 2);
+        dictionary.Add("Circles", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (dictionary.ContainsKey("Triangles"))
+            {
+                dictionary["Triangles"]++;
+                txtTriangle.text = dictionary["Triangles"].ToString();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (dictionary.ContainsKey("Squares"))
+            {
+                dictionary["Squares"]++;
+                txtSquare.text = dictionary["Squares"].ToString();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (dictionary.ContainsKey("Circles"))
+            {
+                dictionary["Circles"]++;
+                txtCircle.text = dictionary["Circles"].ToString();
+            }
+        }
+    }
+}
