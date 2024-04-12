9e56ed8e391821f23b33367e4d666516fd0e6565
JackyWuCoder <JackyWuCoder@gmail.com>
2024-04-12 02:04:20 -0700

    MachineGunEnemy Attack Range

--- Assets/Scripts/Enemies/MachineGunEnemy.cs
+++ Assets/Scripts/Enemies/MachineGunEnemy.cs
@@ -4,6 +4,40 @@ using UnityEngine;
 
 public class MachineGunEnemy : Enemy
 {
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private float attackRangeX = 3f;
+    [SerializeField] private float attackRangeY = 3f;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        if (target != null)
+        {
+            if ((Mathf.Abs(transform.position.x - target.position.x) > attackRangeX) ||
+                (Mathf.Abs(transform.position.y - target.position.y) > attackRangeY))
+            {
+                Move(target.position);
+            }
+            else
+            {
+                Shoot();
+            }
+        }
+        else
+        {
+            Move(speed);
+        }
+    }
+
+    public override void Shoot()
+    {
+        Debug.Log("MachineGunEnemy is Shooting");
+    }
+
     public override void MethodToOverride()
     {
         Debug.Log("Method overridden in MachineGunEnemy");
