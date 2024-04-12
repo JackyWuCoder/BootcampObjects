1ce050403be0bff27b5ea98fc040751dfcc088c4
JackyWuCoder <JackyWuCoder@gmail.com>
2024-04-12 03:17:01 -0700

    Learning Arrays (2)
    1) Instantiate 2 circles and store them into an array then access them and change their transform's position so they don't overlap.

--- Assets/Scripts/DataStructures/ArrayExample.cs
+++ Assets/Scripts/DataStructures/ArrayExample.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class ArrayExample : MonoBehaviour
 {
+    public int num = 5;
     public int[] integerNumbers;
-
     public int[] newIntNumbers = new int[2];
+    public GameObject[] gameObjectArray = new GameObject[2];
+    public GameObject prefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        newIntNumbers[0] = 50;
+        newIntNumbers[1] = -23;
+        // newIntNumbers[2] = 65;
+
+        // Assign prefab as child of the transform's game object
+        gameObjectArray[0] = Instantiate(prefab, transform);
+        // Set position of the prefab after it has been instantiated
+        gameObjectArray[0].transform.position = new Vector2(0, 0);
+        gameObjectArray[1] = Instantiate(prefab, transform);
+        gameObjectArray[1].transform.position = new Vector2(1, 0);
     }
 
     // Update is called once per frame
